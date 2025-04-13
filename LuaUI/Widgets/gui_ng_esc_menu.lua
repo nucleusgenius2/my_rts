596058a886183fb1536fb6063381d2fdeed20f4a
nucleusgenius2 <nucleusgenius@mail.ru>
2025-04-13 12:06:12 +0300

    ручное выравнивание меню паузы, добавление кнопки стопа игры

--- LuaUI/Widgets/gui_ng_esc_menu.lua
+++ LuaUI/Widgets/gui_ng_esc_menu.lua
@@ -2,7 +2,7 @@ function widget:GetInfo()
   return {
     name      = "Pause Menu Modal",
     desc      = "Показывает модальное окно при нажатии Esc, с кнопкой выхода из игры",
-    author    = "ChatGPT + ты",
+    author    = "nucleus genius",
     date      = "2025-04-12",
     license   = "GPLv3",
     layer     = 0,
@@ -12,8 +12,14 @@ end
 
 local Chili
 local screen0
-local window, quitButton
+local window, quitButton,pauseButton
 local isOpen = false
+
+
+--получаем размер окна
+local vsx, vsy = Spring.GetViewGeometry()
+local x = vsx / 2 - 150
+local y = vsy / 2 - 150
 
 function widget:Initialize()
   Chili = WG.Chili
@@ -30,32 +36,52 @@ function widget:Initialize()
     caption     = "Пауза",
     width       = 300,
     height      = 150,
-    x           = "center",
-    y           = "center",
+    x           = x,
+    y           = y,
     draggable   = false,
     resizable   = false,
     visible     = false,
     modal       = true,
+    align="center";
   }
+
+  window:SetVisibility(false) -- закрываем окно по умолчанию
 
   quitButton = Chili.Button:New{
     parent    = window,
     caption   = "Выйти из игры",
-    x         = "center",
+    x         = 40,
     y         = 60,
     width     = 200,
     height    = 40,
     OnClick   = { function()
-      Spring.Echo("exit game")
-      Spring.SendCommands("quit") end 
+      Spring.Echo("лог")
+      Spring.SendCommands("quitforce")
+      end
     },
   }
+
+  pauseButton = Chili.Button:New{
+    parent    = window,
+    caption   = "Пауза",
+    x         = 40,
+    y         = 20,
+    width     = 200,
+    height    = 40,
+    OnClick   = { function()
+      Spring.SendCommands("pause")
+      Spring.Echo("Пауза переключена")
+    end },
+  }
+
 end
 
 function widget:KeyPress(key, mods, isRepeat)
-  if key == 27 then -- Esc
+  if key == 27 then -- Esc кнопка
     isOpen = not isOpen
+    
     if window then
+      Spring.Echo("лог 2")
       window:SetVisibility(isOpen)
     end
     return true
