b7092598c7d7293ae6c35c5be5e2c3782ef790e1
nucleusgenius2 <nucleusgenius@mail.ru>
2025-04-25 22:04:03 +0300

    добавление новой фракции

--- GameData/SideData.lua
+++ GameData/SideData.lua
@@ -1,11 +1,25 @@
 --Wiki: http://springrts.com/wiki/Sidedata.lua
 
-local sideData = {
-	
-	{
-		name = "Vroomers",
-		startUnit = "commander",
-	},
-	
+-- Определение данных для фракций
+local sidedata = {
+    -- Первая фракция
+    [1] = {
+        name = "roomers",  -- Имя фракции
+        startUnit = "commander",  -- Стартовый юнит для фракции
+        sideName = "UEF",  -- Идентификатор фракции
+        --startUnits = {"arm_com", "arm_laser", "arm_solar", "arm_veh_con"},  -- Стартовые юниты
+        sideColor = {0.8, 0.3, 0.3},  -- Цвет для фракции
+        description = "Фракция людей",  -- Описание фракции
+    },
+    -- Вторая фракция
+    [2] = {
+        name = "Core",  -- Имя фракции
+        startUnit = "commander",  -- Стартовый юнит
+        sideName = "CORE",  -- Идентификатор фракции
+        --startUnits = {"cor_com", "cor_laser", "cor_solar", "cor_veh_con"},  -- Стартовые юниты
+        sideColor = {0.3, 0.3, 0.8},  -- Цвет для фракции
+        description = "The Core faction is known for its heavy armor and devastating weapons.",  -- Описание
+    },
 }
-return sideData
+
+return sidedata
