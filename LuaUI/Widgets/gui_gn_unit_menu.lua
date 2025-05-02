c564493bd375101e2b47b79b1098a66f485506e9
nucleusgenius2 <nucleusgenius@mail.ru>
2025-05-02 20:27:54 +0300

    баг с картинкой и стилями в rml

--- LuaUI/Widgets/gui_gn_unit_menu.lua
+++ LuaUI/Widgets/gui_gn_unit_menu.lua
@@ -12,83 +12,27 @@ function widget:GetInfo()
     }
 end
 
--- Для отслеживания выделения
-local prevSelection = {}
-
 local function BuildUnit(_, currentBuilder, buildDefID)
     Spring.Echo("build ---" .. buildDefID)
 
-    -- Проверка на currentBuilder
-    if not currentBuilder then
-        Spring.Echo("currentBuilder is nil!")
-        return
-    end
-
-    -- Проверка на buildDefID
-    if not buildDefID then
-        Spring.Echo("buildDefID is nil!")
-        return
-    end
-
-    -- Получаем координаты мыши
-    local mx, my = Spring.GetMouseState()
-    local type, coords = Spring.TraceScreenRay(mx, my, true)
-
-    -- Проверка, если курсор находится на земле
-    if type == "ground" and coords then
-        local x, y, z = coords[1], coords[2], coords[3]
-
-        -- Логирование координат
-        Spring.Echo("Building unit at: ", x, y, z)
-
-        -- Отправляем команду строительства, которая отображает макет
-    Spring.GiveOrderToUnit(currentBuilder, -buildDefID, {x, y, z}, {"alt", "ctrl", "shift", "right"})
-
-        -- Логирование информации о макете
-        Spring.Echo("Build order sent to currentBuilder:", currentBuilder, "for buildDefID:", buildDefID)
-
+    if currentBuilder then
+        Spring.Echo("currentBuilder: ", currentBuilder)
     else
-        Spring.Echo("Invalid position for building.")
+        Spring.Echo("currentBuilder is nil!")
     end
 end
-local opened = false
-local tries = 0
+
+local prevSelection = {}
 local doc, unitlist
-local currentBuilder
 local main_model_name = "modelunit"
 local init_model = {
-    BuildUnit = BuildUnit,
-    testArray = {
-            { name = "Item 1", id = 1, builderID = 1 },
-            { name = "Item 2", id = 2, builderID = 1 },
-            { name = "Item 3", id = 3, builderID = 1 },
-     },
     message = "тестовое сообщение",
+    testArray = {},
+    hasBuilder = false,
     show = false,
     testblockVisible = false,
-
 }
 local dm_handle
-
-if RmlUi and RmlUi.SetFunction then
-      Spring.Echo("запрос на постройку")
-    RmlUi.SetFunction("BuildUnit", function(_, buildDefID)
-        if currentBuilder and buildDefID then
-            local x, y, z = Spring.GetCameraPosition()
-            Spring.GiveOrderToUnit(currentBuilder, -buildDefID, {x, y, z}, {"shift"})
-            Spring.Echo("Build command issued for buildDefID:", buildDefID)
-        end
-    end)
-end
-
-
-
-
-
---function widget:Initialize()
-  -- Spring.Echo("SelectedUnitsRmlModel: Widget initialized!")
-  -- self:TryOpenContext()
---end
 
 function widget:Initialize()
     widget.rmlContext = RmlUi.GetContext("shared")
@@ -102,20 +46,14 @@ function widget:Initialize()
         Spring.Echo("Failed to load document")
         return
     end
-
-
-
-  -- document:ReloadStyleSheet()
-   document:Show()
+    document:Show()
 end
-
 
 function widget:HideMenu()
     if doc then
-        --doc:Hide()
+        -- doc:Hide()
     end
 end
-
 
 local function sameSelection(a, b)
     if #a ~= #b then return false end
@@ -125,15 +63,10 @@ local function sameSelection(a, b)
     return true
 end
 
---выполняется ра в кадр
 function widget:Update()
-
-
     local selectedUnits = Spring.GetSelectedUnits()
 
     if not sameSelection(selectedUnits, prevSelection) then
-      Spring.Echo("1111119")
-        -- Сохраняем выбор
         prevSelection = {}
         for i = 1, #selectedUnits do
             prevSelection[i] = selectedUnits[i]
@@ -141,80 +74,59 @@ function widget:Update()
 
         Spring.Echo("[SelectedUnitsRmlModel] SelectionChanged: " .. #selectedUnits .. " юнитов выбрано")
 
-    if dm_handle then
-        -- Берём первого выбранного юнита как "билдера"
-        local builderID = selectedUnits[1]
-        currentBuilder = builderID
+        if dm_handle then
+            local unitGroups = {}
+            local hasBuilder = false
 
-        -- buildOptions объект с юнитами которые может построить первый выбранный строитель.
-        local buildOptions = {}
-
-        local unitID = currentBuilder
-        if unitID and Spring.ValidUnitID(unitID) then
-            local unitDefID = Spring.GetUnitDefID(unitID)
-            local unitDef = unitDefID and UnitDefs[unitDefID]
-
-            if unitDef and unitDef.buildOptions then
-                for _, buildDefID in ipairs(unitDef.buildOptions) do
-                    local buildDef = UnitDefs[buildDefID]
-                    if buildDef then
-                        table.insert(buildOptions, {
-                            name = buildDef.humanName or "???",
-                            id = tostring(buildDefID),
-                            icon = "#" .. tostring(buildDefID)
-                        })
+            for _, unitID in ipairs(selectedUnits) do
+                if Spring.ValidUnitID(unitID) then
+                    local unitDefID = Spring.GetUnitDefID(unitID)
+                    local unitDef = unitDefID and UnitDefs[unitDefID]
+                    if unitDef then
+                        if unitDef.isBuilder then
+                            hasBuilder = true
+                        end
+                        unitGroups[unitDefID] = (unitGroups[unitDefID] or 0) + 1
                     end
                 end
             end
-        end
 
-        -- Отдаём в RML
-        local rmlData = {}
+            local rmlData = {}
+            local index = 0
+            for unitDefID, count in pairs(unitGroups) do
+                local unitDef = UnitDefs[unitDefID]
+                if unitDef then
+                    rmlData[index] = {
+                        name = unitDef.humanName or ("UnitDef " .. unitDefID),
+                        icon = "UnitPics/Cleaver.png", -- можно заменить на unitpics/.. если заработает
+                        id = tostring(unitDefID),
+                        count = count,
+                        builderID = "N/A"
+                    }
+                    index = index + 1
+                end
+            end
 
-        for i = 1, #buildOptions do
-            rmlData[i - 1] = {
-                name = buildOptions[i].name,
-                id = buildOptions[i].id,
-                icon = buildOptions[i].icon,
-                builderID = tostring(currentBuilder)
-            }
-        end
-
-        -- Отдаём в RML
-        if #rmlData > 0 then
             dm_handle.testArray = rmlData
-            Spring.Echo("Отправлены все юниты в RML")
-        else
-            Spring.Echo("Нет доступных buildOptions для отправки.")
-        end
-
-
-      else
-             Spring.Echo("Нет widget.dm_handle!")
-       end
-
-     end
-end
-
-
-
-
-_G.ShowTestBlock2 = function(event)
-  Spring.Echo("button message")
-  Spring.Echo("обращение из кнопки")
-    -- Используем переменную-флаг в этом скоупе
-   -- widget.testblockVisible = not widget.testblockVisible
-    --widget:OpenDoc(widget.testblockVisible)
-        if dm_handle then
-             dm_handle.message = "т3333333333333333"
-             dm_handle.testblockVisible = not dm_handle.testblockVisible
-             dm_handle.testArray = {
-                    { name = "22222", value = 1 },
-                    { name = "3333", value = 2 },
-                    { name = "4444", value = 3 },
-             }
-
+            dm_handle.hasBuilder = hasBuilder
+            Spring.Echo("[SelectedUnitsRmlModel] Отправлены данные в RML: " .. tostring(index) .. " типов юнитов")
         else
             Spring.Echo("Нет widget.dm_handle!")
         end
+    end
+end
+
+_G.ShowTestBlock2 = function(event)
+    Spring.Echo("button message")
+    if dm_handle then
+        dm_handle.message = "т3333333333333333"
+        dm_handle.testblockVisible = not dm_handle.testblockVisible
+        dm_handle.testArray = {
+            { name = "22222", value = 1 },
+            { name = "3333", value = 2 },
+            { name = "4444", value = 3 },
+        }
+    else
+        Spring.Echo("Нет widget.dm_handle!")
+    end
 end
