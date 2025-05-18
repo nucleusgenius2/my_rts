9a6e340a42ce8af7cf1c77f36570ef8cbf972a01
nucleusgenius2 <nucleusgenius@mail.ru>
2025-05-18 22:49:34 +0300

    рабочий пример

--- Units/shiledgenerator.lua
+++ Units/shiledgenerator.lua
@@ -21,11 +21,80 @@ local unitDef = {
   explodeAs = "MediumBuildingExplosion",
   selfDestructAs = "MediumBuildingExplosion",
 
-weapons = {
-  [1] = {
-    def = "ShieldGenerator",
+ weapons             = {
+
+    {
+      def                = [[THUD_WEAPON]],
+      badTargetCategory  = [[FIXEDWING]],
+      onlyTargetCategory = [[FIXEDWING LAND SINK TURRET SHIP SWIM FLOAT GUNSHIP HOVER]],
+    },
+
+    {
+      def = [[SHIELD]],
+    },
+
   },
-},
+
+  weaponDefs          = {
+
+    SHIELD      = {
+      name                    = [[Energy Shield]],
+
+      damage                  = {
+        default = 10,
+      },
+
+      exteriorShield          = true,
+      shieldAlpha             = 0.2,
+      shieldBadColor          = [[1 0.1 0.1 1]],
+      shieldGoodColor         = [[0.1 0.1 1 1]],
+      shieldInterceptType     = 3,
+      shieldPower             = 1200,
+      shieldPowerRegen        = 13,
+      shieldPowerRegenEnergy  = 0,
+      shieldRadius            = 300,
+      shieldRepulser          = false,
+      shieldStartingPower     = 1200,
+      smartShield             = true,
+      visibleShield           = false,
+      visibleShieldRepulse    = false,
+      weaponType              = [[Shield]],
+    },
+
+    THUD_WEAPON = {
+      name                    = [[Light Plasma Cannon]],
+      areaOfEffect            = 36,
+      cegTag                  = [[light_plasma_trail]],
+      craterBoost             = 0,
+      craterMult              = 0,
+
+      customParams        = {
+        light_camera_height = 1400,
+        light_color = [[0.80 0.54 0.23]],
+        light_radius = 200,
+      },
+
+      damage                  = {
+        default = 170,
+        planes  = 170,
+      },
+
+      explosionGenerator      = [[custom:MARY_SUE]],
+      impulseBoost            = 0,
+      impulseFactor           = 0.4,
+      interceptedByShieldType = 1,
+      range                   = 280,
+      reloadtime              = 4,
+      soundHit                = [[explosion/ex_med5]],
+      soundStart              = [[weapon/cannon/cannon_fire5]],
+      turret                  = true,
+      weaponType              = [[Cannon]],
+      weaponVelocity          = 210,
+    },
+
+  },
+
+
   customParams = {
     techlevel = 2,
     shield_radius = 300,
