55e50725a3f9899d06442c520f51baa1003ea2c7
nucleusgenius2 <nucleusgenius@mail.ru>
2025-05-24 16:14:03 +0300

    рисует щит, временно решение

--- Weapons/ShieldGenerator.lua
+++ Weapons/ShieldGenerator.lua
@@ -1,30 +1,35 @@
 local weaponName = "ShieldGenerator"
 local weaponDef = {
   name = "Plasma Shield",
-  weaponType = "Shield",
+  --weaponType = "Shield",
+    weaponType              = [[Shield]],
 
-  --damage = {
-   -- default = 100,
-  --},
+  damage = {
+    default = 100,
+  },
 
   shield = {
+    --shieldStartingPower = 5000,
     alpha = 0.35,
     badColor = {1, 0.2, 0.2},
     goodColor = {0.2, 1, 0.2},
     power = 5000,                -- сколько дамага может поглотить
-    powerRegen = 50,            -- восстановление в секунду
-    powerRegenEnergy = 100,     -- энергия на восстановление
+    powerRegen = 150,            -- восстановление в секунду
+   -- powerRegenEnergy = 100,     -- энергия на восстановление
     radius = 300,               -- радиус щита
-    repulser = true,            -- отталкивает снаряды
+    repulser = false,            -- отталкивает снаряды
     smart = true,               -- только от опасных
     visible = true,             -- видно ли купол
     visibleRepulse = true,      -- видно ли отражение
-    --interceptedByShieldType = 1,
-    intercepttype = 479,
+    interceptedByShieldType = 1,
+    ---intercepttype = 479,
+    intercepttype = 1,
+    shieldInterceptType  = 3,
     exteriorShield = true,      -- рисовать снаружи
-
+    shieldRadius = 300,
+    shieldStartingPower = 5000,
+    shieldRepulser          = false,
   },
 
-  shieldRepulser = true,
 }
 return lowerkeys({ [weaponName] = weaponDef })
