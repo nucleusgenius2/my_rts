b463e495b43f97aef342f3dd0858d7ca60412a1c
nucleusgenius2 <nucleusgenius@mail.ru>
2025-05-25 14:20:22 +0300

    скрипт и модель завода едф

--- Objects3d/EDF/buildings/ArmoredPlant.dae.lua
+++ Objects3d/EDF/buildings/ArmoredPlant.dae.lua
@@ -2,7 +2,7 @@ return {
 	radius = 32.0,
 	height = 32.0,
     --tex1 = "Base_Color.png",
-    tex1 = "Zavod.dds",
+    tex1 = "EDF/buildings/ArmoredPlantTech2Edf.dds",
 	--tex2 = "OpenGL.png",
 	midpos = {0,0,0},
 }
